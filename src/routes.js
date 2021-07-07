69efd74c3e3ded160c51443e326c1e92043f7ccf
PaulHaze <dev@artemist.com.au>
2021-07-07 17:25:19 +1000

    clean up

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,7 @@
-import React from 'react';
-import { Home, Test } from './views';
-
 export const routes = [
   {
     path: '/',
     key: 'home',
     label: 'Home',
-    component: Home,
   },
 ];
