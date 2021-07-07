b0aeeca71f75fb52821a9496ae078f6207197f3f
PaulHaze <dev@artemist.com.au>
2021-07-07 17:01:43 +1000

    import views to routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,17 @@
+import React from 'react';
+import { Home, Test } from './views';
+
 export const routes = [
   {
     path: '/',
     key: 'home',
     label: 'Home',
+    component: <Home />,
   },
   {
     path: '/test',
     key: 'test',
     label: 'Test',
+    component: <Test />,
   },
 ];
