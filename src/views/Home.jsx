9054c53eb01bfad4603222e1afb6332539949d92
PaulHaze <dev@artemist.com.au>
2022-10-04 16:04:14 +1100

    init commit

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -8,6 +8,56 @@ export function Home() {
           Delete this and start coding
         </h5>
       </div>
+      <div className="flex items-center justify-center w-8/12 h-40 mx-auto bg-white border rounded shadow-lg px-4 text-center">
+        <h5 className="font-semibold text-coolgray-700">
+          Delete this and start coding
+        </h5>
+      </div>
+      <div className="flex items-center justify-center w-8/12 h-40 mx-auto bg-white border rounded shadow-lg px-4 text-center">
+        <h5 className="font-semibold text-coolgray-700">
+          Delete this and start coding
+        </h5>
+      </div>
+      <div className="flex items-center justify-center w-8/12 h-40 mx-auto bg-white border rounded shadow-lg px-4 text-center">
+        <h5 className="font-semibold text-coolgray-700">
+          Delete this and start coding
+        </h5>
+      </div>
+      <div className="flex items-center justify-center w-8/12 h-40 mx-auto bg-white border rounded shadow-lg px-4 text-center">
+        <h5 className="font-semibold text-coolgray-700">
+          Delete this and start coding
+        </h5>
+      </div>
+      <div className="flex items-center justify-center w-8/12 h-40 mx-auto bg-white border rounded shadow-lg px-4 text-center">
+        <h5 className="font-semibold text-coolgray-700">
+          Delete this and start coding
+        </h5>
+      </div>
+      <div className="flex items-center justify-center w-8/12 h-40 mx-auto bg-white border rounded shadow-lg px-4 text-center">
+        <h5 className="font-semibold text-coolgray-700">
+          Delete this and start coding
+        </h5>
+      </div>
+      <div className="flex items-center justify-center w-8/12 h-40 mx-auto bg-white border rounded shadow-lg px-4 text-center">
+        <h5 className="font-semibold text-coolgray-700">
+          Delete this and start coding
+        </h5>
+      </div>
+      <div className="flex items-center justify-center w-8/12 h-40 mx-auto bg-white border rounded shadow-lg px-4 text-center">
+        <h5 className="font-semibold text-coolgray-700">
+          Delete this and start coding
+        </h5>
+      </div>
+      <div className="flex items-center justify-center w-8/12 h-40 mx-auto bg-white border rounded shadow-lg px-4 text-center">
+        <h5 className="font-semibold text-coolgray-700">
+          Delete this and start coding
+        </h5>
+      </div>
+      <div className="flex items-center justify-center w-8/12 h-40 mx-auto bg-white border rounded shadow-lg px-4 text-center">
+        <h5 className="font-semibold text-coolgray-700">
+          Delete this and start coding
+        </h5>
+      </div>
     </div>
   );
 }
